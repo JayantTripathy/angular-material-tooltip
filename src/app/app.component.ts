457fc112df30e7eaaaeddf8fbe0cfdaf64527f85
JayantTripathy <jayanttripathy1985@gmail.com>
2023-05-02 23:58:50 +0530

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'myApp';
+  // str1 = 'This is string one.';
+  // str2 = '/n' + 'This is string Two.';
+  // str3 = '/n' + 'This is string Three.';
+  // strR = this.str1.concat(this.str2.toString());
+  // strResult = this.strR.concat(this.str3.toString());
+  // tooltipAbovemsg = this.strResult;
+
+  str = 'This is string one.\n This is string Two. \n This is string Three.';
+
+  matTooltipContent = 'Line 1 comment\nLine 2 comment\nLine 3 comment';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from 
+    '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip'; 
+import { MatButtonModule } from '@angular/material/button'; 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+        MatTooltipModule,
+        BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
